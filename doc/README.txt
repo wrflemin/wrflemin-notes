3ed723864847e5cda988740427aba805c3569aea
wrflemin <wrflemin@ualberta.ca>
2014-09-26 14:02:59 -0600

    Added the main file

---
+++ doc/README.txt
@@ -0,0 +1,37 @@
+ToDo-List-App
+=============
+
+   Copyright 2014 Wyatt Fleming
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+A simple to do list application for android for CMPUT 301
+
+This application was built based on the developer tutorials that were
+available on http://developer.android.com/training/index.html and also 
+with the consultation of Alex Makepeace from my CMPUT 301. 
+
+To run this application it can be imported into eclipse with the Android
+SDK installed and run through an emulator or through an actual android phone.
+
+As for the application itself there are many unimplemented features. The 
+application currently takes input from the user in the form of text and 
+puts it into a list below the input field. The list has the option of 
+checking and unchecking the current items that are in the list. If you 
+long click an item, a context menu will appear with a delete option.
+This options is UNIMPLEMENTED, so clicking on the trash icon will not
+delete the list item. Unfortunately the state is not maintained through
+screen orientation changes, so changing orientation will lose the current
+list of to do items. All other features are unimplemented.
+
---
+++ src/ca/ualberta/wrflemin_notes/ToDoListActivityMain.java
@@ -0,0 +1,105 @@
+/*
+    Copyright 2014 Wyatt Fleming
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+
+package ca.ualberta.wrflemin_notes;
+
+import com.example.wrflemin_notes.R;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class ToDoListActivityMain extends ActionBarActivity {
+	//initializes the empty todo list
+	//TODO initialize the old values from storage
+	private ToDoList activeToDoList;
+	private CheckedTextView checkedTextView;
+	private InsertToDoAdapter checkboxAdapter;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_to_do_list_home);
+		
+		activeToDoList = new ToDoList();
+		checkboxAdapter = new InsertToDoAdapter(this,
+		        R.layout.todo_with_checkbox, activeToDoList.getToDoList());
+		
+		
+		ListView listView = (ListView) findViewById(R.id.toDoListView);
+		listView.setAdapter(checkboxAdapter);
+		
+	}
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.to_do_list_home, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	//called when user clicks Add button
+	public void createToDo(View view){
+
+		ToDoItem newToDo;
+		EditText todo_textbox = (EditText) findViewById(R.id.todo_textbox);
+		String toDoText = todo_textbox.getText().toString();
+		if (!toDoText.isEmpty()){
+			todo_textbox.getText().clear();
+			newToDo = new ToDoItem(toDoText, false);
+			activeToDoList.add(newToDo);
+			checkboxAdapter.notifyDataSetChanged();
+		}	
+		
+	}
+	
+	
+	public void checkBox(View view){
+		checkedTextView = (CheckedTextView) view;
+		int position = (Integer)checkedTextView.getTag();
+		
+		if (checkedTextView.isChecked()){
+			checkedTextView.setChecked(false);
+			activeToDoList.getToDo(position).setChecked(false);
+		}
+		else{
+			checkedTextView.setChecked(true);
+			activeToDoList.getToDo(position).setChecked(true);
+		}
+	}
+	
+	
+}
